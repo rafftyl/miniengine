e54c8b4c68baa4f1b6e4a5d9950d83dead6710bf
Wooki <wooki1@op.pl>
2018-01-26 19:38:50 +0100

    Placeholders for game manager, game state and players

--- GameplayMechanics/Source/GameplayManager.h
+++ GameplayMechanics/Source/GameplayManager.h
@@ -1,7 +1,22 @@
 #pragma once
+#include <memory>
+#include <unordered_map>
+#include "GameState\GameState.h"
+#include "Players\DefaultPlayer.h"
 
 
-class GameplayManager
+class GameManager
 {
+public:
+	static GameManager& GetInstance();
+	GameManager(GameManager const&) = delete;
+	void operator=(GameManager const&) = delete;
+	void RunGame();
+	GameState* GetCurrentGameState();
 
+private:
+	GameManager();
+	~GameManager();
+	std::unique_ptr<GameState> currentGameState;
+	std::unordered_map<int, std::unique_ptr<DefaultPlayer>> players;
 };
